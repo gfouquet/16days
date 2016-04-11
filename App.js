fdb49398dad18950b46426a9c608695de7c2747f
Grégory Fouquet <gfouquet@armida.fr>
2016-04-12 00:09:41 +0200

    Webpack : configured src folder, babel for ES6/JSX, eslint

--- App.js
+++ App.js
@@ -1,9 +1,40 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: "lolz"
+    }
+  }
+
   render() {
-    return <div>lol</div>;
+    let update = this.update.bind(this);
+    return (
+      <div>
+        <input type="text" className="type" onChange={update} / >
+        <div>
+        {this.state.txt} {this.props.cat }
+        </div>
+      </div>);
+  }
+
+  update(event) {
+    this.setState({txt: event.target.value})
   }
 }
+
+App.propTypes = {
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  cat: 100
+}
+//
+// ReactDOM.render(
+//   <App txt="lulz" />, document.getElementById('app')
+// );
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,55 @@
+const path = require('path');
+
+const basePath = path.resolve(__dirname);
+
+const config = {
+  paths: {
+    src: path.join(basePath, './src/main/webapp/js'),
+    dist: path.join(basePath, './target/webpack')
+  }
+}
+
+console.log('config', config)
+
 module.exports = {
-  entry: './main.js',
+  // `entry` is a hash -> several bundles with the same config are defined
+  entry: {
+    main: path.join(config.paths.src, './main.js'),
+    admin: path.join(config.paths.src, './admin-main.js')
+  },
   output: {
-    path: './',
-    filename: 'index.js'
+    path: config.paths.dist,
+    // special bracketed filename because we produce several bundles
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].chunk.js'
   },
   devServer: {
     inline: true,
-    port: 3333,
+    port: 8181
   },
   module: {
-    loaders: [
+    preLoaders: [
       {
-        test: /\.js$/,
+        test: /\.js$/, // includes js / jsx files
         exclude: /node_modules/,
+        loader: 'eslint'
+      }
+    ],
+    loaders: [
+      { // babel loader for react / es6
+        test: /\.(js|jsx)$/, // includes js / jsx files
+        exclude: /node_modules/, // excludes npm files
         loader: 'babel',
         query: {
           presets: ['es2015', 'react']
         }
       }
     ]
+  },
+  resolve: {
+    root: config.paths.src,
+    moduleDirectories: ['node_modules'], // by default, also searches bower files
+    extensions: ['', '.js', '.jsx'] // by default, weird extensions + no jsx
   }
 }
+console.log('resolve jsHome', path.resolve('.'))
